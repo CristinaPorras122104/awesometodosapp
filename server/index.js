2541041633f7f78710b6b596c41fd80038020497
Cristina Porras <cristinaporras@example.com>
2025-04-06 14:26:18 +0800

    Move build folder to server directory

--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
-require("dotenv").config();
 const express = require("express");
+const path = require("path");
+require("dotenv").config();
 const { connectToMongoDB } = require("./database");
-const path = require('path');
 
 const app = express();
 app.use(express.json());
 
+// Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
+
+// Home route to serve the index.html from the build folder
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'build/index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const router = require("./routes");
@@ -17,9 +20,10 @@ app.use("/api", router);
 const port = process.env.PORT || 5000;
 
 const startServer = async () => {
-    await connectToMongoDB();
-    app.listen(port, () => {
-        console.log(`Server is listening on http://localhost:${port}`);
-    });
+  await connectToMongoDB();
+  app.listen(port, () => {
+    console.log(`Server is listening on http://localhost:${port}`);
+  });
 };
+
 startServer();
